a62d662398c24b8bdfc926b0b2200cbb8c898e10
teesha.talreja@hotmail.com <xyz.qwe@hotmail.com>
2026-06-18 02:41:16 -0500

    Implemented  addStudent function

--- StudentList.cpp
+++ StudentList.cpp
@@ -4,3 +4,21 @@ using namespace std;
 StudentList::StudentList(){
   head=nullptr;
 }
+
+void StudentList::addStudent(Student s){
+  Node* newNode = new Node;
+  newNode->data = s;
+  newNode->next = nullptr;
+  
+  if(head == nullptr){
+    head = newNode;
+  }
+  else{
+    Node * current = head;
+
+    while(current->next !=nullptr){
+      current = current->next;
+    }
+    current->next = newNode;
+  }
+}
